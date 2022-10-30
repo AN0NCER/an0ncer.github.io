ef5411b30c77be752f36ccc88c6d5f8b99ebc9fd
Anoncer <anoncer@protonmail.com>
2022-10-30 17:54:41 +0100

    Remove Project

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', async event => {
-    console.log("[SW]: Install");
-  });
-  self.addEventListener('activate', async event => {
-    console.log("[SW]: Activate");
-  });
