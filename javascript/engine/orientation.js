64e0b89b0fdc64ebfa12e068fac4009fb5ca2ccb
Anoncer <anoncer@protonmail.com>
2023-03-29 21:26:17 +0200

    Added function Zentrum player on screen 40%

--- javascript/engine/orientation.js
+++ javascript/engine/orientation.js
@@ -1,3 +1,6 @@
+//Текущая ориентация устройства
+let current_device_orientation = "vertical";
+
 (() => {
     //Инидентификатор для каких елементов будет отслеживать изменение ориентации экрана
     const id = "orientationchange";
@@ -6,15 +9,19 @@
 
     if (window.orientation && window.orientation == 90 || window.orientation == -90) {
         elements.addClass("landscape");
+        current_device_orientation = "horizontal";
     } else {
         elements.removeClass("landscape");
+        current_device_orientation = "vertical";
     }
 
     window.addEventListener("orientationchange", function () {
         if (window.orientation && window.orientation == 90 || window.orientation == -90) {
             elements.addClass("landscape");
+            current_device_orientation = "horizontal";
         } else {
             elements.removeClass("landscape");
+            current_device_orientation = "vertical";
         }
     }, false);
 })();
