9292d9c92bcb2f74eeac890744a4f91fe704fb45
Anoncer <anoncer@protonmail.com>
2023-04-02 15:20:42 +0200

    Edit new menu And clean project

--- javascript/jmenu.js
+++ javascript/jmenu.js
@@ -2,15 +2,48 @@
 //Отображен ли меню на сайте
 let application_menu_showed = false;
 //Интерактивное меню устройства
-const application_interactive_html = `<div class="interactive-menu" style="display: none; opacity: 0;"></div>`;
+const application_interactive_html = `<div class="interactive-menu unselectable" style="display: none; ">
+<div class="close-interactive"></div>
+<div class="user-interactive">
+<div class="btn-inter" data-intm="logout"><svg xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512">
+        <path
+            d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
+    </svg>Выход</div>
+<div class="btn-inter" data-intm="updats"><svg xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512">
+        <path
+            d="M142.9 142.9c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5c0 0 0 0 0 0H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5c7.7-21.8 20.2-42.3 37.8-59.8zM16 312v7.6 .7V440c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l41.6-41.6c87.6 86.5 228.7 86.2 315.8-1c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.2 62.2-162.7 62.5-225.3 1L185 329c6.9-6.9 8.9-17.2 5.2-26.2s-12.5-14.8-22.2-14.8H48.4h-.7H40c-13.3 0-24 10.7-24 24z" />
+    </svg>Обновления</div>
+<div class="btn-inter" data-intm="settings"><svg xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512">
+        <path
+            d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" />
+    </svg>Настройки</div>
+</div></div>`;
 //Меню приложения устройства
-const application_menu_html = `<div class="application-menu"></div>`;
+const application_menu_html = `<div class="application-menu unselectable"></div>`;
 //Страниы для переходов
 const application_indexed_page = {
     index: 'index.html',
     list: 'list.html',
     search: 'search.html',
     user: 'user.html'
+};
+//Функции интерактивного меню
+const application_menu_interactive_func = {
+    logout: function(){
+        usr.Storage.Clear();
+        location.reload();
+    },
+
+    updats: function(){
+
+    },
+
+    settings: function(){
+        window.location = application_indexed_page.user;
+    }
 };
 
 //Иконки меню html выбранные и не выбранные
@@ -42,6 +75,8 @@ const application_menu_icon = {
     //Выделяем выбранное страницу в меню
     SelectCurrentMenuPage();
     AddButtonEvents();
+    MenuEventsUser();
+    InteractiveFunctionMenu();
 })();
 
 /**
@@ -99,42 +134,65 @@ function AddButtonEvents() {
     });
 }
 
+/**
+ * Выделяет текущую выбранную страницу
+ */
 function SelectCurrentMenuPage() {
     $('.btn-menu[data-page="' + $($(`head meta[name="page"]`)[0]).attr('content') + '"]').addClass('selected');
 }
 
-// if (window.matchMedia('(display-mode: standalone)').matches || window.matchMedia('(display-mode: fullscreen)').matches) {  
-//     //This is appliction
-//     const html = `<div class="phone_menu">
-//     <div class="btn_menu">
-//         <div class="icon_btn" data-page="index">
-//             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
-//         </div>
-//         <div class="icon_btn" data-page="search">
-//             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>
-//         </div>
-//         <div class="icon_btn" data-page="list">
-//             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-//                 <path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"/>
-//             </svg>
-//         </div>
-//         <div class="icon_btn" data-page="user">
-//             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-//                 <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0S96 57.3 96 128s57.3 128 128 128zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
-//             </svg>
-//         </div>
-//     </div>
-// </div>`;
-// $('body').append(html);
-//     const page = $($(`head meta[name="page"]`)[0]).attr('content');
-//     let el = $('.icon_btn[data-page="'+page+'"]');
-//     if(el.length != 0){
-//         el.addClass('selected');
-//     }
-//     $('.icon_btn').click((h)=>{
-//         let link = $(h.currentTarget).data('page');
-//         if(link != undefined){
-//             window.location.replace('/'+link+'.html');
-//         }
-//     })
-// }  
+/**
+ * Добавляет события к кнопке с пользователем (дополнительные функции)
+ */
+function MenuEventsUser() {
+    let target = '.btn-menu[data-page="user"]';
+    let timer;
+
+    $(target).contextmenu((e) => {
+        e.preventDefault();
+        ShowInteractiveMenu();
+    });
+
+    $(target).bind('touchstart', function (e) {
+        clearInterval(timer);
+        timer = setInterval(() => {
+            ShowInteractiveMenu();
+        }, 700);
+        //e.preventDefault();
+    });
+
+    $(target).bind('touchend', function (e) {
+        clearInterval(timer);
+    });
+
+
+    $('.close-interactive').click((e) => {
+        HideInteractiveMenu();
+    });
+
+    function ShowInteractiveMenu() {
+        $('.interactive-menu').css('display', 'block');
+        setTimeout(() => {
+            $('.interactive-menu').css('opacity', '1');
+        }, 100);
+    }
+
+    function HideInteractiveMenu() {
+        $('.interactive-menu').css('opacity', '0');
+
+        setTimeout(() => {
+            $('.interactive-menu').css({ display: 'none' });
+        }, 300);
+    }
+}
+
+/**
+ * Функционал интеративного меню
+ */
+function InteractiveFunctionMenu(){
+    $('.user-interactive > .btn-inter[data-intm]').click((e)=>{
+        e.preventDefault();
+        let key = $(e.currentTarget).data('intm');
+        application_menu_interactive_func[key]();
+    })
+}
