c582547616f26015b8980fd3c0ddf780e7a4ba8e
Anoncer <anoncer@protonmail.com>
2023-05-19 21:05:58 +0200

    Update cards in searchPage and Fix Bug load recomendation + showng prcnt loading

--- javascript/pages/search.js
+++ javascript/pages/search.js
@@ -290,8 +290,8 @@ function ShowGenres() {
         for (let index = 0; index < response.length; index++) {
             const element = response[index];
             // if (element.kind == 'anime') {
-                $('.genres > .content > .block--' + i).append('<div>' + element.russian + '</div>');
-                i == 1 ? i = 2 : i == 2 ? i = 3 : i = 1;
+            $('.genres > .content > .block--' + i).append('<div>' + element.russian + '</div>');
+            i == 1 ? i = 2 : i == 2 ? i = 3 : i = 1;
             // }
         }
     });
@@ -303,33 +303,26 @@ function ShowHistory() {
         if (data && data.length > 0) {
             //есть данные            
             for (let i = 0; i < data.length; i++) {
-                $('.history > .content').append(`<a href="/watch.html?id=${data[i]}" data-id="${data[i]}"></a>`);
-                loadHistory(data[i]);
+                $('.history > .content').append(`<a href="/watch.html?id=${data[i]}" class="card-anime" data-id="${data[i]}"></a>`);
             }
+            loadHistory(data);
         } else {
             //Истории нет, нужно скрыть вкладку истории
             $('.history').css('display', 'none');
         }
     });
 
-    async function loadHistory(id) {
-        shikimoriApi.Animes.id(id, async (data) => {
-            if (data.failed) {
+    async function loadHistory(list) {
+        shikimoriApi.Animes.animes({ ids: list.toString(), limit: list.length }, async (response) => {
+            if (response.failed) {
                 await sleep(1000);
-                return loadHistory(id);
+                return loadHistory(list);
             }
-            $(`.history > .content > a[data-id="${id}"]`).append(`
-            <div class="card-anime" data-anime="${data.id}">
-                <div class="content-img">
-                    <div class="saved"></div>
-                    <div class="title">${data.russian}</div>
-                    <img src="https://nyaa.shikimori.me${data.image.original}" alt="${data.russian}">
-                </div>
-                <div class="content-inf">
-                    <div class="inf-year">${new Date(data.aired_on).getFullYear()}</div>
-                    <div class="inf-rtng"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path></svg>${data.score}</div>
-                </div>
-            </div>`);
+
+            for (let i = 0; i < response.length; i++) {
+                const element = response[i];
+                $(`.history > .content > a[data-id="${element.id}"]`).append(GenerateCardAnime(element));   
+            }
         });
     }
 }
@@ -369,9 +362,9 @@ async function Recomendation(logged) {
         for (let i = 0; i < saveSimiliar.length; i++) {
             //Добавляем пустышку
             element.append(EmptyAnime(saveSimiliar[i]));
-            //Делаем загрузку
-            LazyLoadAnime(saveSimiliar[i]);
         }
+        //Делаем загрузку
+        LazyLoadAnimes(saveSimiliar);
 
         Status("");
     }
@@ -399,28 +392,19 @@ async function Recomendation(logged) {
         for (let i = 0; i < saveSimiliar.length; i++) {
             //Добавляем пустышку
             element.append(EmptyAnime(saveSimiliar[i]));
-            //Делаем загрузку
-            LazyLoadAnime(saveSimiliar[i]);
         }
+        //Делаем загрузку
+        LazyLoadAnimes(saveSimiliar);
     } else {
+        //Очищаем наш список рекомендаций
+        $(`${dom} > .content`).empty();
         for (let i = 0; i < saveSimiliar.length; i++) {
             const id = saveSimiliar[i];
-            const a = $($(dom + '>.content>a')[i]);
-            //Если елемент отличается, изменяем
-            if (a) {
-                if (id != a.data('id')) {
-                    a.empty();
-                    a.data('href', `/watch.html?id=${id}`);
-                    a.data('id', id);
-                    LazyLoadAnime(id);
-                }
-            } else {
-                const element = $(dom + '> .content');
-                element.append(EmptyAnime(id));
-                LazyLoadAnime(id);
-            }
-
+            const element = $(dom + '> .content');
+            element.append(EmptyAnime(id));
         }
+        //Делаем загрузку
+        LazyLoadAnimes(saveSimiliar);
     }
 
     Status("обновлено");
@@ -441,35 +425,29 @@ async function Recomendation(logged) {
         element.text(text);
     }
 
-    //Функция загрузка аниме по id и вывод на экран
-    async function LazyLoadAnime(id) {
+    //Загружает список аниме и выводит на заготовленное для них место
+    async function LazyLoadAnimes(list = []) {
         if (pasuse) {
             await sleep(2000);
-            return LazyLoadAnime(id);
+            return LazyLoadAnimes(list);
         }
-        shikimoriApi.Animes.id(id, async (data) => {
-            if (data.failed) {
+
+        shikimoriApi.Animes.animes({ ids: list.toString(), limit: list.length }, async (response) => {
+            if (response.failed && response.status == 429) {
                 await sleep(1000);
-                return LazyLoadAnime(id);
+                return await LazyLoadAnimes(list);
             }
-            $(`a[data-id="${id}"]`).append(`
-            <div class="card-anime" data-anime="${data.id}">
-                <div class="content-img">
-                    <div class="saved"></div>
-                    <div class="title">${data.russian}</div>
-                    <img src="https://nyaa.shikimori.me${data.image.original}" alt="${data.russian}">
-                </div>
-                <div class="content-inf">
-                    <div class="inf-year">${new Date(data.aired_on).getFullYear()}</div>
-                    <div class="inf-rtng"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"></path></svg>${data.score}</div>
-                </div>
-            </div>`);
+
+            for (let i = 0; i < response.length; i++) {
+                const element = response[i];
+                $(`a[data-id="${element.id}"]`).append(GenerateCardAnime(element));
+            }
         });
     }
 
     //Функция возвращает пустышку аниме для lazy-loading
     function EmptyAnime(id) {
-        return `<a href="/watch.html?id=${id}" data-id="${id}"></a>`;
+        return `<a href="/watch.html?id=${id}" class="card-anime" data-id="${id}"></a>`;
     }
 
     //Функция для получение список просмотренных аниме пользователя
@@ -505,6 +483,7 @@ async function Recomendation(logged) {
         for (let i = 0; i < response.length; i++) {
             const e = response[i];
             let d = await getSimmiliar(e.target_id);
+            Status(`обновляем (${Math.floor(calculatePercentage(i + 1, response.length))}%)`);
             d.forEach(e => {
                 //Проверяем не находится ли id в исключении 
                 if (!ignores.find(x => x == e.id)) {
@@ -551,6 +530,10 @@ async function Recomendation(logged) {
             saveSimiliar = s;
             localStorage.setItem('save-similar', JSON.stringify(saveSimiliar));
         }
+    }
+
+    function calculatePercentage(currentElement, totalElements) {
+        return (currentElement / totalElements) * 100;
     }
 }
 
@@ -608,6 +591,10 @@ function trackElementReachEnd(className, callback) {
             callback();
         }
     });
+}
+
+function GenerateCardAnime(data){
+    return `<div class="card-content"><img src="https://moe.shikimori.me/${data.image.original}"><div class="title"><span>${data.russian}</span></div></div><div class="card-information"><div class="year">${new Date(data.aired_on).getFullYear()}</div><div class="score"><svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.73196 0.745728C4.65834 0.595337 4.50279 0.499634 4.33196 0.499634C4.16112 0.499634 4.00696 0.595337 3.93196 0.745728L3.0389 2.55452L1.04446 2.84436C0.877789 2.86897 0.7389 2.98381 0.687511 3.14104C0.636122 3.29827 0.677789 3.4719 0.797233 3.58811L2.24446 4.99768L1.90279 6.98967C1.87501 7.15374 1.94446 7.32053 2.08196 7.4176C2.21946 7.51467 2.4014 7.52698 2.5514 7.44905L4.33334 6.51252L6.11529 7.44905C6.26529 7.52698 6.44723 7.51604 6.58473 7.4176C6.72223 7.31917 6.79168 7.15374 6.7639 6.98967L6.42084 4.99768L7.86807 3.58811C7.98751 3.4719 8.03057 3.29827 7.97779 3.14104C7.92501 2.98381 7.78751 2.86897 7.62084 2.84436L5.62501 2.55452L4.73196 0.745728Z" fill="#FFE600"/></svg>${data.score}</div></div>`;
 }
 
 function sleep(ms) {
