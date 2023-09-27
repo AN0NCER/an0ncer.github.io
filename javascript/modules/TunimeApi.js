6dc218228850253a22952c6365fd25fb2a720ff1
Anoncer <anoncer@protonmail.com>
2023-09-27 20:56:47 +0200

    Orginize ApiTunime

--- javascript/modules/TunimeApi.js
+++ javascript/modules/TunimeApi.js
@@ -1,29 +1,39 @@
 let _updateData = undefined;
 let _enabled = false;
 
-
 GetOnlineData();
 setInterval(() => {
     GetOnlineData();
 }, 299000);
 
 function GetOnlineData() {
-    fetch('https://tunime.onrender.com/online').then((respnose) => {
-        return respnose.json();
+    fetch('https://tunime.onrender.com/online').then((response) => {
+        return response.json();
     }).then((val) => {
         _updateData = val;
         _enabled = true;
     });
 }
 
-export function GetM3U8(url) {
-    return new Promise((resolve) => {
-        fetch("https://anime-m3u8.onrender.com/link-anime", { body: new URLSearchParams({ 'link': 'https:' + url }), method: 'post' })
-            .then(function (response) { return response.json(); })
-            .then(function (data) {
-                return resolve(data);
-            }).catch((res)=>{
-                console.error(res);
-            });
-    });
+export const ApiTunime = {
+    anime: async function (id) {
+        fetch(`https://tunime.onrender.com/online/${id}/o`).then((response) => {
+            return response.json();
+        }).then((val) => {
+            console.log(val);
+        });
+    },
+
+    online: function () {
+
+    },
+
+    stream: function (url) {
+        return new Promise((resolve) => {
+            fetch(`https://anime-m3u8.onrender.com/link-anime`, { body: new URLSearchParams({ 'link': 'https:' + url }), method: 'post' })
+                .then(function (response) { return response.json(); })
+                .then(function (data) { return resolve(data); })
+                .catch((res) => { console.log(res) });
+        });
+    }
 }
