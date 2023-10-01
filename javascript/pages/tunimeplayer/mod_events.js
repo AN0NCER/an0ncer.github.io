3ad36239eb0c32f1a31f167d17adb1cf18045337
Anoncer <anoncer@protonmail.com>
2023-10-01 19:12:33 +0200

    Fix player (Not Load Image)

---
+++ javascript/pages/tunimeplayer/mod_events.js
@@ -0,0 +1,56 @@
+import { ParentWindow, VideoPlayer } from "../tunimeplayer.js";
+
+export function RegEvents() {
+    VideoPlayer.addEventListener("error", (e) => {
+        ParentWindow.postMessage({ key: 'tunime_error' }, "*");
+    });
+    VideoPlayer.addEventListener("loadedmetadata", () => {
+    });
+
+    VideoPlayer.addEventListener("pause", () => {
+        ParentWindow.postMessage({ key: 'kodik_player_pause' }, '*');
+    });
+
+    VideoPlayer.addEventListener("play", () => {
+        ParentWindow.postMessage({ key: 'kodik_player_play' }, '*');
+        _elistPlay.forEach((x) => {
+            x();
+        })
+    });
+
+    VideoPlayer.addEventListener("timeupdate", () => {
+        ParentWindow.postMessage({ key: 'kodik_player_time_update', value: Math.floor(VideoPlayer.currentTime) }, '*');
+        var minutes = Math.floor(VideoPlayer.currentTime / 60);
+        var seconds = Math.floor(VideoPlayer.currentTime - minutes * 60)
+        var x = minutes < 10 ? "0" + minutes : minutes;
+        var y = seconds < 10 ? "0" + seconds : seconds;
+        $('.current-time').text(`${x}:${y}`);
+    });
+
+    VideoPlayer.addEventListener("durationchange", () => {
+        ParentWindow.postMessage({ key: 'kodik_player_duration_update', value: VideoPlayer.duration }, '*');
+        var minutes = Math.floor(VideoPlayer.duration / 60);
+        var seconds = Math.floor(VideoPlayer.duration - minutes * 60)
+        var x = minutes < 10 ? "0" + minutes : minutes;
+        var y = seconds < 10 ? "0" + seconds : seconds;
+        $('.video-duration').text(`${x}:${y}`);
+    });
+
+    VideoPlayer.addEventListener("ended", () => {
+        ParentWindow.postMessage({ key: 'kodik_player_video_ended' }, '*');
+    });
+
+    VideoPlayer.addEventListener('volumechange', () => {
+        ParentWindow.postMessage({ key: 'kodik_player_volume_change', value: { muted: VideoPlayer.muted, volume: VideoPlayer.volume } }, '*');
+    });
+}
+
+
+//Список с событиями
+const _elistPlay = [];
+
+export function OnPlayerPlay(e = () => { }) {
+    if (typeof e == "function") {
+        _elistPlay.push(e);
+    }
+}
