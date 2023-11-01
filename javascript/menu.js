57ef80b354b16f2e13f81dffceb57f7edca34c2d
Anoncer <anoncer@protonmail.com>
2023-11-01 19:42:11 +0100

    Autonekst

--- javascript/menu.js
+++ javascript/menu.js
@@ -492,15 +492,15 @@ const IntercatMenu = {
         standartcontrol: {
             type: "parametr",
             disabled: () => {
-                return !$PARAMETERS.player.standart;
+                return false;
             },
             value: () => {
-                return $PARAMETERS.player.standart_controls;
+                return $PARAMETERS.player.autonekst;
             },
-            text: "Стандартное управление",
+            text: "Автопереключение",
             icon: `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>`,
             event: (bool) => {
-                setParameter('standart_controls', bool);
+                setParameter('autonekst', bool);
             }
         },
         fullscreen: {
