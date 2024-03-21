8151a444a8465c070c0d91e7b0671e1d1617dfbe
Anoncer <anoncer@protonmail.com>
2024-03-21 14:16:36 +0100

    Fixed a bug with the number of episodes exceeding its number Fixed a bug with loading anime that used the url of a local page

--- javascript/pages/watch/mod_download.js
+++ javascript/pages/watch/mod_download.js
@@ -29,7 +29,7 @@ function LoadingEpisodes() {
         return;
     }
     loaded = true;
-    const e = Player().episodes.episodes_count;
+    const e = Player().episodes.last_episode;
     for (let i = 0; i < e; i++) {
         const count = i + 1;
         $('.window-body-fs > .download-episode > .down-value').append(`<span class="down-episode" data-index="${count}">${count}<span class="ep-name">EP</span><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM376.9 294.6L269.8 394.5c-3.8 3.5-8.7 5.5-13.8 5.5s-10.1-2-13.8-5.5L135.1 294.6c-4.5-4.2-7.1-10.1-7.1-16.3c0-12.3 10-22.3 22.3-22.3l57.7 0 0-96c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 96 57.7 0c12.3 0 22.3 10 22.3 22.3c0 6.2-2.6 12.1-7.1 16.3z"/></svg></span>`);
@@ -185,25 +185,26 @@ function _localDownload(data) {
     const url = data[quality][0].src.indexOf("http") != -1 ? data[quality][0].src : "https:" + data[quality][0].src;
     // Строка, которую нужно удалить
     const searchString = `${quality}.mp4:hls:manifest.m3u8`;
-    // Удалите подстроку из URL
-    const urlkodik = url.substring(0, url.indexOf(searchString));
+
 
     fetch(url)
-        .then(response => response.text())
-        .then(async (m3u8Content) => {
-            // data содержит текст манифеста M3U8
-            const tsUrls = m3u8Content.split('\n').filter(line => line.trim().endsWith('.ts'));
+        .then(response => {
+            // Удалите подстроку из URL
+            const urlkodik = response.url.substring(0, response.url.indexOf(searchString));
+            response.text().then(async (m3u8Content) => {
+                // data содержит текст манифеста M3U8
+                const tsUrls = m3u8Content.split('\n').filter(line => line.trim().endsWith('.ts'));
+                // Сохраняем время начала загрузки
+                startTime = new Date().getTime();
 
-            // Сохраняем время начала загрузки
-            startTime = new Date().getTime();
-
-            if ($PARAMETERS.download.dasync) {
-                AsyncDownloadVideo(tsUrls, urlkodik);
-            } else {
-                DownloadVideo(tsUrls, urlkodik);
-            }
-        }).catch(error => {
-            console.error('Ошибка при загрузке M3U8: ', error);
+                if ($PARAMETERS.download.dasync) {
+                    AsyncDownloadVideo(tsUrls, urlkodik);
+                } else {
+                    DownloadVideo(tsUrls, urlkodik);
+                }
+            }).catch(error => {
+                console.error('Ошибка при загрузке M3U8: ', error);
+            });
         });
 }
 
