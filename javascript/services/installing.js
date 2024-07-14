172829d3e09ee02b2c36ce5dafbaa082aa6c2ef9
Anoncer <anoncer@protonmail.com>
2024-07-14 14:52:48 +0200

    fix redirect when install pwa app

--- javascript/services/installing.js
+++ javascript/services/installing.js
@@ -1,5 +1,3 @@
-import { Manager } from "./dispatcher.js";
-
 const key = 'application_installed';
 
 let app = JSON.parse(localStorage.getItem(key)) || {
@@ -13,6 +11,6 @@ let app = JSON.parse(localStorage.getItem(key)) || {
         app.installed = true;
         app.date = new Date().toISOString();
         localStorage.setItem(key, JSON.stringify(app));
-        Manager.NewTask('on-install', 'pageload').data({ redirect: 'login.html', page: 'index' });
+        window.location.href = 'login.html';
     }
 })();
