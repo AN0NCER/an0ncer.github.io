e9a2e0e92463b79dd18cd82f69845a323d769c89
Anoncer <anoncer@protonmail.com>
2025-07-19 01:07:51 +0200

    [#141] Remove old`s localStorage keys

--- javascript/modules/Settings.js
+++ javascript/modules/Settings.js
@@ -12,8 +12,3 @@ export class SHIKIURL {
         return `${this.protocol}${sub}.${this.domain}`;
     }
 }
-
-export const AppStorageKeys = {
-    "lastUpdate": 'dialog-update', //{"show":false,"ver":"2.5.1","hash":"69c3d","update":"2024-10-27T23:32:24.597Z"}
-    "gitVersion": 'github-version'
-}
